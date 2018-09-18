6ea91d2df902fd944435cf5676b266ada18e9847
Norbertlm <noobertlm@gmail.com>
2018-09-17 21:30:20 -0400

    clean up spacing and flake8

--- condTests.py
+++ condTests.py
@@ -6,13 +6,25 @@ from evalTokens import evaluateTokens
 class TestCond(unittest.TestCase):
 
     def test_cond_basic(self):
-        exp = tokenize("(define cats 5)(define dogs 6)(define birds 5)(cond [(eq? cats birds)(+ cats birds)][(eq? cats dogs)(+ cats dogs)][else 0])")
+        exp = tokenize("(define cats 5)"
+                       "(define dogs 6)"
+                       "(define birds 5)"
+                       "(cond "
+                       "[(eq? cats birds)(+ cats birds)]"
+                       "[(eq? cats dogs)(+ cats dogs)]"
+                       "[else 0])")
         result = evaluateTokens(exp)
         expected = 10
         self.assertEqual(result, expected)
 
     def test_cond_else(self):
-        exp = tokenize("(define cats 1)(define dogs 2)(define birds 3)(cond [(eq? cats birds) (+ cats birds)][(eq? cats dogs)(+ cats dogs)][else 0])")
+        exp = tokenize("(define cats 1)"
+                       "(define dogs 2)"
+                       "(define birds 3)"
+                       "(cond "
+                       "[(eq? cats birds)(+ cats birds)]"
+                       "[(eq? cats dogs)(+ cats dogs)]"
+                       "[else 0])")
         result = evaluateTokens(exp)
         expected = 0
         self.assertEqual(result, expected)
--- defineTests.py
+++ defineTests.py
@@ -6,17 +6,14 @@ from evalTokens import evaluateTokens
 class TestDefine(unittest.TestCase):
 
     def test_define_basic(self):
-        ls = tokenize("(define cat 5)")
-        evaluateTokens(ls)
+        evaluateTokens(tokenize("(define cat 5)"))
         result = evaluateTokens(tokenize("(+ cat cat)"))
         expected = 10
         self.assertEqual(result, expected)
 
     def test_define_nest(self):
         evaluateTokens(tokenize("(define rat (+ 6 11))"))
-        ls = tokenize("(- rat 4)")
-        result = evaluateTokens(ls)
-
+        result = evaluateTokens(tokenize("(- rat 4)"))
         expected = 13
         self.assertEqual(result, expected)
 
