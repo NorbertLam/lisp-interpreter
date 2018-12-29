3c5444a503c965d9f5bbe91920ada7788a3f0b61
Norbertlm <noobertlm@gmail.com>
2018-12-28 23:48:34 -0500

    refactor main

--- src/main.py
+++ src/main.py
@@ -10,9 +10,7 @@ parser.add_argument('filename', nargs='?', type=argparse.FileType('r'))
 args = parser.parse_args()
 
 if args.filename:
-    with args.filename as file:
-        for line in file:
-            print(evaluate_multiple_expression(tokenize(line)))
+    print(evaluate_multiple_expression(tokenize(args.filename.read())))
 elif args.command:
     print(evaluate_multiple_expression(tokenize(args.command)))
 else:
