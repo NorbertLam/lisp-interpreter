bb82aa77992a2df48d29604b6e35795dbbdc10ac
Norbertlm <noobertlm@gmail.com>
2019-01-03 20:54:15 -0500

    add __eq__

--- src/parseTree.py
+++ src/parseTree.py
@@ -12,6 +12,9 @@ class DefineNode(ParseNode):
     def __str__(self):
         return "DefineNode(" + str(self.name) + ", " + str(self.expr) + ")"
 
+    def __eq__(self, other):
+        return isinstance(other, DefineNode) and self.name == other.name and self.expr == other.expr
+
     def evaluate(self):
         expression_for_id[self.name] = self.expr.evaluate()
 
@@ -22,6 +25,9 @@ class NumberNode(ParseNode):
 
     def __str__(self):
         return "NumberNode(" + str(self.number) + ")"
+
+    def __eq__(self, other):
+        return isinstance(other, NumberNode) and self.number == other.number
 
     def evaluate(self):
         return self.number
@@ -34,6 +40,9 @@ class BooleanNode(ParseNode):
     def __str__(self):
         return "BooleanNode(" + str(self.boolean) + ")"
 
+    def __eq__(self, other):
+        return isinstance(other, BooleanNode) and self.boolean == other.boolean
+
     def evaluate(self):
         return self.boolean
 
@@ -45,6 +54,9 @@ class IdNode(ParseNode):
     def __str__(self):
         return "IdNode(" + str(self.i_d) + ")"
 
+    def __eq__(self, other):
+        return isinstance(other, IdNode) and self.i_d == other.i_d
+
     def evaluate(self):
         return expression_for_id[self.i_d]
 
@@ -55,6 +67,9 @@ class CondNode(ParseNode):
 
     def __str__(self):
         return "CondNode(" + str(self.cond_expressions) + ")"
+
+    def __eq__(self, other):
+        return isinstance(other, CondNode) and self.cond_expressions == other.cond_expressions
 
     def evaluate(self):
         for cond, expression in self.cond_expressions:
@@ -69,6 +84,9 @@ class PrintNode(ParseNode):
     def __str__(self):
         return "PrintNode(" + str(self.expr) + ")"
 
+    def __eq__(self, other):
+        return isinstance(other, PrintNode) and self.expr == other.expr
+
     def evaluate(self):
         print(self.expr.evaluate())
 
@@ -80,7 +98,11 @@ class BinaryFunctionNode(ParseNode):
         self.expr2 = expr2
 
     def __str__(self):
-        return "BinaryFunctionNode(" + str(self.expr1) + ", " + str(self.expr2) + ")"
+        return "BinaryFunctionNode(" + str(self.func) + ", " + str(self.expr1) + ", " + str(self.expr2) + ")"
+
+    def __eq__(self, other):
+        return isinstance(other, BinaryFunctionNode) and self.func == other.func and self.expr1 == other.expr1 \
+            and self.expr2 == other.expr2
 
     def evaluate(self):
         return self.func(self.expr1.evaluate(), self.expr2.evaluate())
@@ -92,7 +114,10 @@ class UnaryFunctionNode(ParseNode):
         self.expr = expr
 
     def __str__(self):
-        return "UnaryFunctionNode(" + str(self.expr) + ")"
+        return "UnaryFunctionNode(" + str(self.func) + ", " + str(self.expr) + ")"
+
+    def __eq__(self, other):
+        return isinstance(other, UnaryFunctionNode) and self.func == other.func and self.expr == other.expr
 
     def evaluate(self):
         return self.func(self.expr.evaluate())
